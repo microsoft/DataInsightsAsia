ef5b579c36ba071ccf579286ae1d73c7ed032260
Matt <matt.lavery@outlook.com>
2018-01-09 15:16:16 +1000

    Added support for SQL Auth

--- Scripts/ExportAndMigration/ExportAgentJobs.ps1
+++ Scripts/ExportAndMigration/ExportAgentJobs.ps1
@@ -9,6 +9,9 @@ Param(
 
     , [Parameter(mandatory=$false)]
     [string]$Path = "C:\Output"
+    ,
+    [Parameter(Mandatory=$false)]
+    [pscredential]$SqlCredentials
 )
 
 # make sure the output path exists
@@ -17,13 +20,34 @@ if (!(Test-Path -Path $Path))
     New-Item -Path $Path -ItemType Directory | Out-Null;
 }
 
-#load the required assemblies via the provided script
-Push-Location;
-Import-Module SQLPS;
-Pop-Location;
+# Import the SQL Module to load the assemblies
+if ($null -ne (Get-module -Name SqlServer -ListAvailable -ErrorAction SilentlyContinue))
+{
+    Import-Module -Name SqlServer;
+}
+elseif ($null -ne (Get-module -Name SQLPS -ListAvailable -ErrorAction SilentlyContinue)) 
+{
+    Push-Location;
+    Import-Module -Name SQLPS -DisableNameChecking;
+    Pop-Location;
+}
+else 
+{
+    Write-Error "Neither the SqlServer or SQLPS PowerShell Modules are installed"
+    #throw $PSItem;
+    Exit; # stop the script execution
+}
 
 # Create the SMO object
 $smoServer = New-Object Microsoft.SqlServer.Management.Smo.Server($SqlServer);
+
+# if we need to provide SQL Auth credential
+if ($null -ne $SqlCredentials)
+{
+    $smoServer.ConnectionContext.LoginSecure = $false;
+    $smoServer.ConnectionContext.Login = $SqlCredentials.UserName;
+    $smoServer.ConnectionContext.Password = $SqlCredentials.GetNetworkCredential().Password;
+}
 
 # Create a Scripter object and set the required scripting options.
 $scrp = New-Object -TypeName Microsoft.SqlServer.Management.SMO.Scripter -ArgumentList ($smoServer);
