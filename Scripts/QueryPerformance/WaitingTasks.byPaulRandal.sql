049f92d2c8e0d56fa306dce0837382b22ce0db47
Matt Lavery <mlavery@microsoft.com>
2018-06-18 23:47:44 +1000

    Updated headers

--- Scripts/QueryPerformance/WaitingTasks.byPaulRandal.sql
+++ Scripts/QueryPerformance/WaitingTasks.byPaulRandal.sql
@@ -1,27 +1,18 @@
-/*********************************************************************************************
-Written By: Matt Lavery
-Date:		12/10/2016
-Purpose:	Provides the details of the top waiting tasks in a SQL Server instance. 
-            
-            The script was created by Paul Randal http://www.sqlskills.com/blogs/paul/wait-statistics-or-please-tell-me-where-it-hurts/
-
-Changes:
-Who			When		What
-MLavery		12/10/2016	Updated script from sqlskills.com
-
-Disclaimer:
-This Sample Code is provided for the purpose of illustration only and is not intended to be 
-used in a production environment.  THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED 
-"AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
-TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  We grant 
-You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and 
-distribute the object code form of the Sample Code, provided that You agree: (i) to not use 
-Our name, logo, or trademarks to market Your software product in which the Sample Code is 
-embedded; (ii) to include a valid copyright notice on Your software product in which the 
-Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our 
-suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise 
-or result from the use or distribution of the Sample Code.
-*********************************************************************************************/
+--
+--  Author:        Matt Lavery
+--  Date:          12/10/2016
+--  Purpose:       Provides the details of the top waiting tasks in a SQL Server instance. 
+--                 The script was created by Paul Randal http://www.sqlskills.com/blogs/paul/wait-statistics-or-please-tell-me-where-it-hurts/
+-- 
+--  Version:       0.1.0 
+--  Disclaimer:    This script is provided "as is" in accordance with the projects license
+--
+--  History
+--  When        Version     Who         What
+--  -----------------------------------------------------------------
+--  12/10/2016  0.1.1       mlavery     Updated script from sqlskills.com
+--  -----------------------------------------------------------------
+--
 
 WITH [Waits] AS
     (SELECT
